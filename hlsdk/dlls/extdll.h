0d01591526bc5ef9ca0477bc5178d50e8726b3d8
BlackPhrase <BlackPhrase@users.noreply.gitlab.com>
2021-05-08 23:06:50 +0000

    Fix the def/type redefinition errors

--- hlsdk/dlls/extdll.h
+++ hlsdk/dlls/extdll.h
@@ -59,15 +59,15 @@ typedef int BOOL;
 #include "math.h"
 
 // Header file containing definition of globalvars_t and entvars_t
-typedef int	func_t;					//
-typedef int	string_t;				// from engine's pr_comp.h;
+//typedef int	func_t;					//
+//typedef int	string_t;				// from engine's pr_comp.h;
 typedef float vec_t;				// needed before including progdefs.h
 
 // Vector class
 #include "vector.h"
 
 // Defining it as a (bogus) struct helps enforce type-checking
-#define vec3_t Vector
+//#define vec3_t Vector
 
 // Shared engine/DLL constants
 #include "const.h"
